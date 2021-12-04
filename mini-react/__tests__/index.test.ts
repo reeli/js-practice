1274937f37beada21188572ae164cf3447e1f862
reeli <reeli.null@gmail.com>
2021-12-04 17:04:21 +0800

    diff children

--- mini-react/__tests__/index.test.ts
+++ mini-react/__tests__/index.test.ts
@@ -26,4 +26,65 @@ describe('createElement', () => {
 
     expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`)
   });
+
+  it('should render VNode to html with VNode children', () => {
+    const div = document.createElement("div")
+    const child = createElement("span", {id: "content"}, ["my content"]);
+
+    render(div, createElement("div", {id: "my-div"}, [child]));
+    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`)
+
+    const preDiv = div.querySelector("#my-div")
+
+    render(div, createElement("div", {id: "my-div-2"}, [child]));
+    expect(div.innerHTML).toEqual(`<div id="my-div-2"><span id="content">my content</span></div>`)
+
+    expect(div.querySelector("#my-div-2")).toEqual(preDiv);
+  });
+
+  it('should render new added attributes', () => {
+    const div = document.createElement("div")
+    const child = createElement("span", {id: "content"}, ["my content"]);
+
+    render(div, createElement("div", {id: "my-div"}, [child]));
+    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`)
+
+    render(div, createElement("div", {id: "my-div-2", name: "good"}, [child]));
+    expect(div.innerHTML).toEqual(`<div id="my-div-2" name="good"><span id="content">my content</span></div>`)
+  });
+
+  it('should diff child', () => {
+    const div = document.createElement("div")
+    const child1 = createElement("span", {id: "content"}, ["my content"]);
+
+    render(div, createElement("div", {id: "my-div"}, [child1]));
+    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`)
+
+    const child2 = createElement("span", {id: "content2"}, ["my content2"]);
+
+    render(div, createElement("div", {id: "my-div-2"}, [child2]));
+    expect(div.innerHTML).toEqual(`<div id="my-div-2"><span id="content2">my content2</span></div>`)
+  });
+
+  it('should diff child', () => {
+    const root = document.createElement("div")
+    const child1 = createElement("span", {id: "content1"}, ["1"]);
+    const child2 = createElement("span", {id: "content2"}, ["2"]);
+
+    render(root, createElement("div", {id: "root"}, [child1, child2]));
+    expect(root.innerHTML).toEqual(`<div id="root"><span id="content1">1</span><span id="content2">2</span></div>`)
+
+    const prevContent1 = root.querySelector("#content1");
+    const prevContent2 = root.querySelector("#content2");
+
+    const child3 = createElement("div", {id: "content3"}, ["3"]);
+    render(root, createElement("div", {id: "div-3"}, [child3, child2]));
+    const currentContent2 = root.querySelector("#content2");
+    const currentContent3 = root.querySelector("#content3");
+
+    expect(root.innerHTML).toEqual(`<div id="div-3"><div id="content3">3</div><span id="content2">2</span></div>`)
+
+    expect(prevContent1).not.toEqual(currentContent3);
+    expect(prevContent2).toEqual(currentContent2);
+  });
 });
--- mini-react/index.ts
+++ mini-react/index.ts
@@ -8,11 +8,12 @@ interface VNode {
   children?: ChildNode[] | null;
 }
 
+const isObject = (data: any)=> typeof data === "object" && data !== null
 
 export const createElement = (type: string, props: AnyObject, children?: ChildNode[]): VNode => {
   function mapChildren(children: ChildNode[]) {
-    return children.map((child: ChildNode)=>{
-      if (typeof child === "object" && child !== null) {
+    return children.map((child: ChildNode) => {
+      if (isObject(child)) {
         return child;
       }
 
@@ -27,22 +28,80 @@ export const createElement = (type: string, props: AnyObject, children?: ChildNo
   }
 }
 
-export const render = (rootElement: HTMLElement, content: VNode) => {
+const isEqual = (a: AnyObject, b: AnyObject): boolean => {
+  return JSON.stringify(a) === JSON.stringify(b);
+}
+
+const diff = (rootElement: HTMLElement & {vDOM?: VNode; html?: HTMLElement}, prev: VNode, current: VNode) => {
+  if (prev.type !== current.type) {
+    rootElement.html && rootElement.removeChild(rootElement.html);
+    renderContent(rootElement, current);
+    return;
+  }
+
+  if (!isEqual(prev.props, current.props)) {
+    for (const k1 in prev.props){
+      const currentKeys = Object.keys(current.props);
+      if(currentKeys.includes(k1)){
+        if(prev.props[k1] !== current.props[k1]){
+          const child = rootElement.html;
+          child && child.setAttribute(k1, current.props[k1]);
+        }
+      }else{
+        const child = rootElement.html;
+        child && child.removeAttribute(k1);
+      }
+    }
+
+    const newAddedKeys = Object.keys(current.props).filter((v)=> !Object.keys(prev.props).includes(v));
+    const child = rootElement.html;
+
+    newAddedKeys.forEach(k=>{
+      child && child.setAttribute(k, current.props[k]);
+    });
+  }
+
+  prev.children?.forEach((v, idx) => {
+    if(isObject(v) && current.children && isObject(current.children[idx])){
+      diff((rootElement?.html as any), v as VNode, current.children[idx] as VNode)
+      return;
+    }
+
+    if(current.children && !isObject(current.children[idx])){
+      const content = document.createTextNode(current.children[idx] as any);
+      (rootElement?.html as any)?.replaceChildren(content);
+    }
+  })
+}
+
+
+const renderContent = (rootElement:HTMLElement & {vDOM?: VNode; html?: HTMLElement}, content:VNode)=>{
   const element = document.createElement(content.type);
 
   for (const k in content.props) {
     element.setAttribute(k, content.props[k]);
   }
 
-
-  content.children?.forEach((v)=>{
-    if(typeof v==="string"){
+  content.children?.forEach((v) => {
+    if (typeof v === "string") {
       const textNode = document.createTextNode(v);
       element.appendChild(textNode);
-    }else{
-      render(element, v as VNode);
+    } else {
+      renderContent(element, v as VNode);
     }
   })
 
+  rootElement.vDOM = content;
+  rootElement.html = element;
+
   rootElement.appendChild(element)
+}
+
+export const render = (rootElement: HTMLElement & {vDOM?: VNode; html?: HTMLElement}, content: VNode) => {
+  if (rootElement.vDOM) {
+    diff(rootElement, rootElement.vDOM, content);
+    return;
+  }
+
+  renderContent(rootElement, content);
 }
