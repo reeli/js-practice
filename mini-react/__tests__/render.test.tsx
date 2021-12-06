3c4bc2715f7e65cdeb80ea96727b6b2d193ac7fd
reeli <reeli.null@gmail.com>
2021-12-06 17:46:23 +0800

    update create-element

--- mini-react/__tests__/render.test.tsx
+++ mini-react/__tests__/render.test.tsx
@@ -1,99 +1,96 @@
 // @ts-nocheck
-import {createElement} from "../create-element";
-import {render} from "../render"
+import { createElement } from "../create-element";
+import { render } from "../render";
 
-describe('render', () => {
-  it('should render VNode to html', () => {
-    const div = document.createElement("div")
-    render(div, createElement("div", {id: "my-div"}, [123, 456, "hello-world"]));
+describe("render", () => {
+  it("should render VNode to html", () => {
+    const div = document.createElement("div");
+    render(div, createElement("div", { id: "my-div" }, [123, 456, "hello-world"]));
 
-    expect(div.innerHTML).toEqual(`<div id="my-div">123456hello-world</div>`)
+    expect(div.innerHTML).toEqual(`<div id="my-div">123456hello-world</div>`);
   });
 
-  it('should render VNode to html with VNode children', () => {
-    const div = document.createElement("div")
-    const child = createElement("span", {id: "content"}, ["my content"]);
-    render(div, createElement("div", {id: "my-div"}, [child]));
+  it("should render VNode to html with VNode children", () => {
+    const div = document.createElement("div");
+    const child = createElement("span", { id: "content" }, ["my content"]);
+    render(div, createElement("div", { id: "my-div" }, [child]));
 
-    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`)
+    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`);
   });
 
-  it('should render VNode to html with VNode children', () => {
-    const div = document.createElement("div")
-    const child = createElement("span", {id: "content"}, ["my content"]);
+  it("should render VNode to html with VNode children", () => {
+    const div = document.createElement("div");
+    const child = createElement("span", { id: "content" }, ["my content"]);
 
-    render(div, createElement("div", {id: "my-div"}, [child]));
-    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`)
+    render(div, createElement("div", { id: "my-div" }, [child]));
+    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`);
 
-    const preDiv = div.querySelector("#my-div")
+    const preDiv = div.querySelector("#my-div");
 
-    render(div, createElement("div", {id: "my-div-2"}, [child]));
-    expect(div.innerHTML).toEqual(`<div id="my-div-2"><span id="content">my content</span></div>`)
+    render(div, createElement("div", { id: "my-div-2" }, [child]));
+    expect(div.innerHTML).toEqual(`<div id="my-div-2"><span id="content">my content</span></div>`);
 
     expect(div.querySelector("#my-div-2")).toEqual(preDiv);
   });
 
-  it('should render new added attributes', () => {
-    const div = document.createElement("div")
-    const child = createElement("span", {id: "content"}, ["my content"]);
+  it("should render new added attributes", () => {
+    const div = document.createElement("div");
+    const child = createElement("span", { id: "content" }, ["my content"]);
 
-    render(div, createElement("div", {id: "my-div"}, [child]));
-    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`)
+    render(div, createElement("div", { id: "my-div" }, [child]));
+    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`);
 
-    render(div, createElement("div", {id: "my-div-2", name: "good"}, [child]));
-    expect(div.innerHTML).toEqual(`<div id="my-div-2" name="good"><span id="content">my content</span></div>`)
+    render(div, createElement("div", { id: "my-div-2", name: "good" }, [child]));
+    expect(div.innerHTML).toEqual(`<div id="my-div-2" name="good"><span id="content">my content</span></div>`);
   });
 
-  it('should diff single child', () => {
-    const div = document.createElement("div")
-    const child1 = createElement("span", {id: "content"}, ["my content"]);
+  it("should diff single child", () => {
+    const div = document.createElement("div");
+    const child1 = createElement("span", { id: "content" }, ["my content"]);
 
-    render(div, createElement("div", {id: "my-div"}, [child1]));
-    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`)
+    render(div, createElement("div", { id: "my-div" }, [child1]));
+    expect(div.innerHTML).toEqual(`<div id="my-div"><span id="content">my content</span></div>`);
 
-    const child2 = createElement("span", {id: "content2"}, ["my content2"]);
+    const child2 = createElement("span", { id: "content2" }, ["my content2"]);
 
-    render(div, createElement("div", {id: "my-div-2"}, [child2]));
-    expect(div.innerHTML).toEqual(`<div id="my-div-2"><span id="content2">my content2</span></div>`)
+    render(div, createElement("div", { id: "my-div-2" }, [child2]));
+    expect(div.innerHTML).toEqual(`<div id="my-div-2"><span id="content2">my content2</span></div>`);
   });
 
-  it('should diff with multiple child', () => {
-    const root = document.createElement("div")
-    const child1 = createElement("span", {id: "content1"}, ["1"]);
-    const child2 = createElement("span", {id: "content2"}, ["2"]);
+  it("should diff with multiple child", () => {
+    const root = document.createElement("div");
+    const child1 = createElement("span", { id: "content1" }, ["1"]);
+    const child2 = createElement("span", { id: "content2" }, ["2"]);
 
-    render(root, createElement("div", {id: "root"}, [child1, child2]));
-    expect(root.innerHTML).toEqual(`<div id="root"><span id="content1">1</span><span id="content2">2</span></div>`)
+    render(root, createElement("div", { id: "root" }, [child1, child2]));
+    expect(root.innerHTML).toEqual(`<div id="root"><span id="content1">1</span><span id="content2">2</span></div>`);
 
     const prevContent1 = root.querySelector("#content1");
     const prevContent2 = root.querySelector("#content2");
 
-    const child3 = createElement("div", {id: "content3"}, ["3"]);
-    render(root, createElement("div", {id: "div-3"}, [child3, child2]));
+    const child3 = createElement("div", { id: "content3" }, ["3"]);
+    render(root, createElement("div", { id: "div-3" }, [child3, child2]));
     const currentContent2 = root.querySelector("#content2");
     const currentContent3 = root.querySelector("#content3");
 
     expect(prevContent1).not.toEqual(currentContent3);
     expect(prevContent2).toEqual(currentContent2);
 
-    expect(root.innerHTML).toEqual(`<div id="div-3"><div id="content3">3</div><span id="content2">2</span></div>`)
+    expect(root.innerHTML).toEqual(`<div id="div-3"><div id="content3">3</div><span id="content2">2</span></div>`);
   });
 
-  it('should diff components', () => {
+  it("should diff components", () => {
     const root = document.createElement("div");
     const Foo = (props) => {
-      return createElement("div", {id: "foo"}, [
-        createElement("span", null, [props.greet]),
-        props.children
-      ])
+      return createElement("div", { id: "foo" }, [createElement("span", null, [props.greet]), props.children]);
     };
 
-    render(root, createElement(Foo, {greet: "hello"}, ["content1"]));
+    render(root, createElement(Foo, { greet: "hello" }, ["content1"]));
     // const prevContent = root.querySelector("#foo");
-    expect(root.innerHTML).toEqual(`<div id="foo"><span>hello</span>content1</div>`)
+    expect(root.innerHTML).toEqual(`<div id="foo"><span>hello</span>content1</div>`);
 
-    render(root, createElement(Foo, {greet:"good"}, ["content2"]));
-    expect(root.innerHTML).toEqual(`<div><div id="foo"><span>good</span>content2</div></div>`)
+    render(root, createElement(Foo, { greet: "good" }, ["content2"]));
+    expect(root.innerHTML).toEqual(`<div id="foo"><span>good</span>content2</div>`);
     // const currentContent = root.querySelector("#foo");
     //
     // expect(prevContent).toEqual(currentContent);
--- mini-react/create-element.ts
+++ mini-react/create-element.ts
@@ -1,36 +1,20 @@
-import {VNode, VChildNode} from "./types";
-import {isObject, flat} from "./utils";
+import { VNode, VChildNode } from "./types";
+import { flat } from "./utils";
 
-export const createElement = (type: string | Function, props: AnyObject, children?: VChildNode[]|null): VNode => {
+export const createElement = (type: string | Function, props: AnyObject, children?: VChildNode[] | null): VNode => {
   if (typeof type === "function") {
     return {
       type,
       props: {
         ...props,
-        children: children ? mapChildren(children) as VChildNode[] : null
-      }
-    }
-  }
-
-  function mapChildren(children: VChildNode[]): VChildNode[] {
-    const res = children.map((child: VChildNode) => {
-      if (Array.isArray(child)) {
-        return mapChildren(child);
-      }
-
-      if (isObject(child)) {
-        return child;
-      }
-// <Foo>{false}</Foo> => <div>false</div>
-      return String(child);
-    });
-
-    return flat(res);
+        children: children ? flat(children) : null,
+      },
+    };
   }
 
   return {
     type,
     props,
-    children: children ? mapChildren(children) : null
-  }
-}
+    children: children ? flat(children) : null,
+  };
+};
